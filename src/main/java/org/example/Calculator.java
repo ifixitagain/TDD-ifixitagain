1a06d264dfe470eaba098ef7e9d6ff1cc922c919
ceciliastude <cecilia.stude@ha.ax>
2025-10-23 08:56:50 +0300

    Skapat flera test

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -6,4 +6,7 @@ public class Calculator {
     {
         return a+b;
     }
+    public static int subtract(int a, int b){return a-b;}
+    public static int divide(int a, int b){return a/b;}
+    public static int multiply(int a, int b){return a*b;}
 }
--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -16,4 +16,37 @@ public void testAddTwoPositiveNumbers()
     int actualSum = Calculator.add(a,b);
     assertEquals(expectedSum, actualSum);
 }
+
+@Test
+    public void testSubtractnumbers()
+{
+    int  a=10;
+    int  b=5;
+    int expectedSum = 5;
+
+    int actualSum = Calculator.subtract(a,b);
+    assertEquals(expectedSum, actualSum);
+}
+
+@Test
+    public void testDivideNumbers()
+{
+    int  a=10;
+    int  b=5;
+    int expectedSum = 2;
+
+    int actualSum = Calculator.divide(a,b);
+    assertEquals(expectedSum, actualSum);
+}
+
+@Test
+    public void testMultiplication()
+{
+    int  a=10;
+    int  b=2;
+    int expectedSum = 20;
+
+    int actualSum = Calculator.multiply(a,b);
+    assertEquals(expectedSum, actualSum);
+}
 }
